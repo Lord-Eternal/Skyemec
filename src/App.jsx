584e6308d16e12d895f4289ef59a749253f06977
Lord-Eternal <et3rnxl24@gmail.com>
2025-05-13 20:50:33 +0700

    login added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./user_fontend/components/Header";
 import Body from "./user_fontend/components/Body";
-import React from "./login/components/Body";
+import LoginBody from "./login/components/Body";
+import LoginHeader from "./login/components/Header";
 function App() {
   return (
     <>
